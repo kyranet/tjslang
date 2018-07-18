3aa719a8c9f643404ff05670314c2562e191b15f
kyraNET <kyradiscord@gmail.com>
2018-07-18 16:50:24 +0200

    ci: Added Travis.yml for CI

--- test/basic.js
+++ test/basic.js
@@ -1,7 +1,8 @@
 const { Parser } = require('../src/index');
 const parsed = new Parser().parse(require('fs').readFileSync(require('path').join(__dirname, 'basic.tjs'), 'utf8'));
-// console.log(parsed.exports.LANGUAGE.PERMISSION.RESTRICTED_HELP.render()({ command: 'eval', role: '@everyone' }));
-// console.log(parsed.exports);
-console.log(parsed.render());
+
+const rendered = parsed.render();
+console.log(rendered);
+
 // eslint-disable-next-line new-cap
-console.log(parsed.render().LANGUAGE.PERMISSION.RESTRICTED_HELP({ command: 'eval', role: 'Core Developers' }));
+console.log(rendered.LANGUAGE.PERMISSION.RESTRICTED_HELP({ command: 'eval', role: 'Core Developers' }));
